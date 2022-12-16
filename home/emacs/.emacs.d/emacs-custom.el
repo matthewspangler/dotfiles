48dea70dd22334b9ad85a65ad5a55c7805cbe93d
Matt Spangler <mattspangler@protonmail.com>
2022-12-15 20:25:31 -0600

    Improvements to emacs config

--- home/emacs/.emacs.d/emacs-custom.el
+++ home/emacs/.emacs.d/emacs-custom.el
@@ -1,1 +1,19 @@
 ;;; For some reason this blank file is required to setup Emacs on Windows.
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-trello-current-prefix-keybinding "C-c o" nil (org-trello))
+ '(org-trello-files '("~/Documents/Org-Notebooks/miningstore-trello.org") nil (org-trello))
+ '(safe-local-variable-values
+   '((eval org-babel-lob-ingest "./accounting.org")
+     (eval org-babel-lob-ingest "./portfolio_builder.org")
+     (org-confirm-babel-evaluate)))
+ '(zoom-size '(0.618 . 0.618)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
